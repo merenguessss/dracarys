508321983d88ca4fdece049c42aa3d97c9508fc4
merengues <2681377430@qq.com>
2021-05-23 09:32:33 +0800

    feat: 完善server_options相关配置的载入

--- transport/server_transport_options.go
+++ transport/server_transport_options.go
@@ -9,11 +9,35 @@ type ServerOptions struct {
 	network         Network
 	address         string
 	keepAlivePeriod time.Duration
-	handle          Handler
+	handler         Handler
 }
 
 type ServerOption func(*ServerOptions)
 
 type Handler interface {
 	handle(context.Context, []byte) ([]byte, error)
+}
+
+func WithNetwork(network string) ServerOption {
+	return func(so *ServerOptions) {
+		so.network = Network(network)
+	}
+}
+
+func WithAddress(addr string) ServerOption {
+	return func(so *ServerOptions) {
+		so.address = addr
+	}
+}
+
+func WithKeepAlivePeriod(t time.Duration) ServerOption {
+	return func(so *ServerOptions) {
+		so.keepAlivePeriod = t
+	}
+}
+
+func WithHandler(handler Handler) ServerOption {
+	return func(so *ServerOptions) {
+		so.handler = handler
+	}
 }
