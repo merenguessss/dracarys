76bd680f0a298ee46e1162ff5f5e7c09879a913c
merengues <2681377430@qq.com>
2021-06-10 20:27:33 +0800

    fix: 修改example中的client和server.

--- example/client/client.go
+++ example/client/client.go
@@ -11,10 +11,12 @@ func main() {
 	opts := []client.Option{
 		client.WithAddr("localhost:8000"),
 		client.WithNetWork("tcp"),
+		client.WithSerializerType("json"),
+		client.WithCodecType("proto"),
 	}
 	c := dracarys.NewClient(opts...)
 	c.Service("Hello")
-	res, err := c.Call("World", "1111")
+	res, err := c.Call("World", "1111", "1222")
 	if err != nil {
 		fmt.Println(err)
 	}
