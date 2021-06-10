271f7263a4a9debd70261566524feddbd7eaf2c0
merengues <2681377430@qq.com>
2021-06-10 20:30:02 +0800

    feat: 增加Client端的调用方式.

--- client.go
+++ client.go
@@ -2,6 +2,8 @@ package dracarys
 
 import (
 	"context"
+	"errors"
+	"strings"
 
 	"github.com/merenguessss/dracarys-go/client"
 )
@@ -10,6 +12,8 @@ type Client struct {
 	c    client.Client
 	opts []client.Option
 }
+
+type Method func(...interface{}) (interface{}, error)
 
 func NewClient(opts ...client.Option) *Client {
 	return &Client{
@@ -22,7 +26,37 @@ func (c *Client) Service(name string) {
 	c.opts = append(c.opts, client.WithService(name))
 }
 
-func (c *Client) Call(methodName string, req interface{}) (interface{}, error) {
+// Call 通过MethodName定位请求到具体的req.
+func (c *Client) Call(methodName string, req ...interface{}) (interface{}, error) {
 	c.opts = append(c.opts, client.WithMethod(methodName))
-	return c.c.Invoke(context.Background(), req, "", c.opts...)
+	return c.c.Invoke(context.Background(), req, c.opts...)
+}
+
+// Method 获取具体Method.
+func (c *Client) Method(name string) Method {
+	c.opts = append(c.opts, client.WithMethod(name))
+	return func(req ...interface{}) (interface{}, error) {
+		return c.c.Invoke(context.Background(), req, c.opts...)
+	}
+}
+
+// ServiceAndMethod 通过service和method查询函数.
+func (c *Client) ServiceAndMethod(name string) (Method, error) {
+	serviceName, methodName, err := c.parseServicePath(name)
+	if err != nil {
+		return nil, err
+	}
+	c.opts = append(c.opts, client.WithService(serviceName), client.WithMethod(methodName))
+	return func(req ...interface{}) (interface{}, error) {
+		return c.c.Invoke(context.Background(), req, c.opts...)
+	}, nil
+}
+
+// 解析serviceName和MethodName.
+func (c *Client) parseServicePath(path string) (string, string, error) {
+	index := strings.LastIndex(path, "/")
+	if index == 0 || index == -1 || !strings.HasPrefix(path, "dracarys.service.") {
+		return "", "", errors.New("invalid path")
+	}
+	return path[0:index], path[index+1:], nil
 }
