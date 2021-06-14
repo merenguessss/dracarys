73c7b696b8facaf40477660b30005edb82ef3431
merengues <2681377430@qq.com>
2021-06-14 10:48:52 +0800

    feat: 增加插件方面的配置文件加载。并修复了配置文件不存在时报错的问题，现在配置文件不存在则返回默认配置.

--- config/config.go
+++ config/config.go
@@ -1,19 +1,23 @@
 package config
 
 import (
+	"errors"
 	"io/ioutil"
 	"os"
 	"sync/atomic"
 	"time"
 
 	"github.com/merenguessss/dracarys-go/client"
+	"github.com/merenguessss/dracarys-go/plugin"
 	"github.com/merenguessss/dracarys-go/server"
+
 	"gopkg.in/yaml.v3"
 )
 
 var configStream []byte
 var path string
 var config = newDefault()
+var ErrorConfigFileNotExist = errors.New("config file not exist")
 
 // Setting 整体设置结构体.
 // Client 客户端默认设置.
@@ -21,6 +25,7 @@ var config = newDefault()
 type Setting struct {
 	Client *client.Options `yaml:"client"`
 	Server *server.Options `yaml:"server"`
+	Plugin *plugin.Options `yaml:"plugin"`
 	isLoad int32
 }
 
@@ -37,6 +42,7 @@ var newDefault = func() *Setting {
 			SerializerType:  "json",
 			CodecType:       "proto",
 		},
+		Plugin: plugin.DefaultConfig(),
 	}
 }
 
@@ -51,7 +57,20 @@ func readConfigBytes() ([]byte, error) {
 			path = c
 		}
 	}
+
+	if pathExist(path) {
+		return nil, ErrorConfigFileNotExist
+	}
 	return ioutil.ReadFile(path)
+}
+
+// pathExist 判断是否存在config文件.
+func pathExist(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		return os.IsNotExist(err)
+	}
+	return true
 }
 
 // SetPath 设置自定义的path路径.
@@ -76,19 +95,27 @@ func loadConfig() error {
 		return nil
 	}
 
+	pluginFactory := plugin.New(config.Plugin)
+	config.Client.PluginFactory = pluginFactory
+	config.Server.PluginFactory = pluginFactory
+
 	var err error
 	if configStream == nil {
 		configStream, err = readConfigBytes()
 		if err != nil {
+			// 如果config文件不存在,直接返回默认配置.
+			if err == ErrorConfigFileNotExist {
+				return nil
+			}
 			return err
 		}
 	}
 
 	err = yaml.Unmarshal(configStream, config)
-	atomic.AddInt32(&config.isLoad, 1)
 	if err != nil {
 		return err
 	}
+	atomic.AddInt32(&config.isLoad, 1)
 	return nil
 }
 
